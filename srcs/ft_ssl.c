cf98aa70f8eeac5d5351756766db4a094e6db9c0
Florian Cadet <cadet.florian@gmail.com>
2022-10-22 08:57:15 +0200

    fully working with subject tests

--- srcs/ft_ssl.c
+++ srcs/ft_ssl.c
@@ -6,7 +6,7 @@
 /*   By: fcadet <fcadet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 16:52:32 by fcadet            #+#    #+#             */
-/*   Updated: 2022/10/21 10:47:34 by fcadet           ###   ########.fr       */
+/*   Updated: 2022/10/22 08:55:02 by fcadet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -158,31 +158,32 @@ uint8_t			*stdin_2_mem(uint128_t *sz) {
 	return (mem);
 }
 
-err_t		proc_stdin(alg_t alg, opts_t opts) {
+err_t		proc_stdin(alg_t alg, opts_t opts, int argc) {
 	char		*hash;
 	uint8_t		*stdin_mem;
 	uint128_t	mem_sz;
 
-	if (!isatty(0)) {
-		if (opt_isset(opts, O_QUIET)
-			|| !opt_isset(opts, O_PRINT)) {
-			if (!(hash = hash_file(alg, stdin)))
-				return (error(E_HASH_STDIN));
-			if (opt_isset(opts, O_QUIET))
-				printf("%s\n", hash);
-			else
-				printf("(stdin) = %s\n", hash);
-		} else {
-			if (!(stdin_mem = stdin_2_mem(&mem_sz)))
-				return (error(E_HASH_STDIN));
-			if (!(hash = hash_mem(alg, stdin_mem, mem_sz))) {
-				free(stdin_mem);
-				return (error(E_HASH_STDIN));
-			}
+	if (opt_isset(opts, O_PRINT)) {
+		if (!(stdin_mem = stdin_2_mem(&mem_sz)))
+			return (error(E_HASH_STDIN));
+		if (!(hash = hash_mem(alg, stdin_mem, mem_sz))) {
+			free(stdin_mem);
+			return (error(E_HASH_STDIN));
+		}
+		if (opt_isset(opts, O_QUIET))
+			printf("%.*s%s\n", (int)mem_sz, stdin_mem, hash);
+		else {
 			for (; mem_sz && stdin_mem[mem_sz - 1] == '\n'; --mem_sz);
 			printf("(\"%.*s\") = %s\n", (int)mem_sz, stdin_mem, hash);
-			free(stdin_mem);
 		}
+		free(stdin_mem);
+	} else if (!opt_isset(opts, O_STR) && !argc) {
+		if (!(hash = hash_file(alg, stdin)))
+			return (error(E_HASH_STDIN));
+		if (opt_isset(opts, O_QUIET))
+			printf("%s\n", hash);
+		else
+			printf("(stdin) = %s\n", hash);
 	}
 	return (E_NO_ERR);
 }
@@ -193,13 +194,17 @@ err_t		proc_str(alg_t alg, opts_t opts, int *argc, char ***argv) {
 	if (opt_isset(opts, O_STR)) {
 		if (!argc)
 			return (error(E_NO_STR));
-		if (!(hash = hash_mem(alg, (uint8_t *)*argv, strlen(**argv))))
+		if (!(hash = hash_mem(alg, (uint8_t *)**argv, strlen(**argv))))
 			return (error(E_HASH_MEM));
 		if (opt_isset(opts, O_QUIET))
 			printf("%s\n", hash);
-		else
-			printf("%s (\"%s\") = %s\n",
-					to_upper(ALG_STR[alg]), **argv, hash);
+		else {
+			if (opt_isset(opts, O_REV))
+				printf("%s \"%s\"\n", hash, **argv);
+			else
+				printf("%s (\"%s\") = %s\n",
+						to_upper(ALG_STR[alg]), **argv, hash);
+		}
 		--*argc;
 		++*argv;
 	}
@@ -209,13 +214,15 @@ err_t		proc_str(alg_t alg, opts_t opts, int *argc, char ***argv) {
 err_t		proc_files(alg_t alg, opts_t opts, int *argc, char ***argv) {
 	FILE		*file;
 	char		*hash;
+	err_t		err = 0;
 
 	for (; *argc; --*argc, ++*argv) {
 		if (!(file = fopen(**argv, "r"))
 			|| !(hash = hash_file(alg, file))) {
+			err |= error(E_HASH_FILE, **argv);
 			if (file)
 				fclose(file);
-			return (error(E_HASH_FILE, **argv));
+			continue;
 		}
 		fclose(file);
 		if (opt_isset(opts, O_QUIET))
@@ -243,7 +250,7 @@ int			main(int argc, char **argv) {
 		return (error(E_UNK_ALG, unk_alg));
 	if ((dup_opt = parse_opts(&opts, &argc, &argv)))
 		return (error(E_DUP_OPT, dup_opt));
-	err |= err_2_flag(proc_stdin(alg, opts));
+	err |= err_2_flag(proc_stdin(alg, opts, argc));
 	err |= err_2_flag(proc_str(alg, opts, &argc, &argv));
 	err |= err_2_flag(proc_files(alg, opts, &argc, &argv));
 	return (err);
